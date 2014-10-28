4af8988d3505caf9177c2ddacdfe2b58216a85f8
stelescuraul <stelescuraul@gmail.com>
2014-10-28 22:36:05 +0100

    Added functionality for clicking a customer.
    Missing the background color.
    
    Needs revision if we can come with a better solution.

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var Customer = require('../models/customer');
 
 /* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource');
+router.get('/:id', function (req, res) {
+    var customerId = req.params.id;
+    Customer.getCustomer(customerId, function (err, c) {
+        if (err) {
+            res.status(500).send({status: 500, message: err.message, type: 'internal'});
+            res.end();
+            return;
+        }
+        console.log("------------------------------")
+        console.log(c);
+        res.setHeader('Content-Type', 'text/html');
+        res.render("users");
+    });
+});
+
+router.get('/getuser/:id', function (req, res) {
+    var customerId = req.params.id;
+    Customer.getCustomer(customerId, function (err, c) {
+        if (err) {
+            res.status(500).send({status: 500, message: err.message, type: 'internal'});
+            res.end();
+            return;
+        }
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(c));
+    });
 });
 
 module.exports = router;
