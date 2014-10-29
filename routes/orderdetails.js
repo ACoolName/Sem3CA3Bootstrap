eb8bed5a49bf17b34d1dc9409a86b2f42aa9ec1e
TeodorDumitru <TeoDum@gmail.com>
2014-10-29 11:48:36 +0100

    Fixed the product names.

--- routes/orderdetails.js
+++ routes/orderdetails.js
@@ -28,11 +28,15 @@ router.get('/:id', function (req, res) {
                 Product.getAllProducts(productIds, function (err, products) {
                     if (!errorHandler.errorHandle(err, res)) return;
                     Employee.getEmployee(o.employeeId, function (err, employee) {
+                        var hashMap = {};
+                        products.forEach(function(prod) {
+                            hashMap[prod._id] = prod;
+                        });
                         var fullOrder = {
                             customer: c,
                             order: o,
                             orderDetails: orderDetails,
-                            products: products,
+                            products: hashMap,
                             employee: employee
                         };
                         res.setHeader('Content-Type', 'application/json');
