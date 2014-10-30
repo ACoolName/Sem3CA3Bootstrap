c57efd560554871ea977822e89cd3667255acbf9
TeodorDumitru <TeoDum@gmail.com>
2014-10-30 15:51:45 +0100

    Removed all the script code from the .jade files and added it into the side.js file.

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -2,16 +2,17 @@ function populateProductsTable() {
     // Empty content string
     var tableContent = '';
     // jQuery AJAX call for JSON
-    $.getJSON( '/products/all', function( data ) {
-        function compare(a,b) {
+    $.getJSON('/products/all', function (data) {
+        function compare(a, b) {
             if (a._id < b._id)
                 return -1;
             if (a._id > b._id)
                 return 1;
             return 0;
         }
+
         data.sort(compare);
-        $.each(data, function(){
+        $.each(data, function () {
             tableContent += '<tr>';
             tableContent += '<td>' + this._id + '</td>';
             tableContent += '<td>' + this.name + '</td>';
@@ -32,16 +33,17 @@ function populateCustomersTable() {
     // Empty content string
     var tableContent = '';
     // jQuery AJAX call for JSON
-    $.getJSON('/customer/', function( data ) {
-        function compare(a,b) {
+    $.getJSON('/customer/', function (data) {
+        function compare(a, b) {
             if (a._id < b._id)
                 return -1;
             if (a._id > b._id)
                 return 1;
             return 0;
         }
+
         data.sort(compare);
-        $.each(data, function(){
+        $.each(data, function () {
             tableContent += '<tr>';
             tableContent += '<td>' + this._id + '</td>';
             tableContent += '<td>' + this.companyName + '</td>';
@@ -66,4 +68,236 @@ function editAndDelete() {
     tableContent += '<td>' + '<button type="button" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-edit"></span></button>' + '</td>';
     tableContent += '<td>' + '<button type="button" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-remove-circle"></span></button>' + '</td>';
     return tableContent;
+}
+
+function goBack() {
+    window.history.back();
+}
+
+function populateEmployeesTable() {
+    // Empty content string
+    var tableContent = '';
+    // jQuery AJAX call for JSON
+    $.getJSON('/employee/all', function (data) {
+        function compare(a, b) {
+            if (a._id < b._id)
+                return -1;
+            if (a._id > b._id)
+                return 1;
+            return 0;
+        }
+
+        data.sort(compare);
+        $.each(data, function () {
+            tableContent += '<tr>';
+            tableContent += '<td>' + this._id + '</td>';
+            tableContent += '<td>' + this.lastName + '</td>';
+            tableContent += '<td>' + this.firstName + '</td>';
+            tableContent += '<td>' + this.title + '</td>';
+            tableContent += '<td>' + this.titleOfCourtesy + '</td>';
+            tableContent += '<td>' + this.birthDate + '</td>';
+            tableContent += '<td>' + this.hireDate + '</td>';
+            tableContent += '<td>' + this.address + '</td>';
+            tableContent += '<td>' + this.region + '</td>';
+            tableContent += '<td>' + this.postalCode + '</td>';
+            tableContent += '<td>' + this.country + '</td>';
+            tableContent += '<td>' + this.homePhone + '</td>';
+            tableContent += '<td>' + this.extension + '</td>';
+            tableContent += '<td>' + this.notes + '</td>';
+            tableContent += '</tr>';
+        });
+        // Inject the whole content string into our existing HTML table
+        $('#employeesViewer table tbody').html(tableContent);
+    });
+}
+
+function populateOrderTable() {
+    // Empty content string
+    var tableContent = '';
+    // jQuery AJAX call for JSON
+    $.getJSON('/order', function (data) {
+        function compare(a, b) {
+            if (a._id > b._id)
+                return -1;
+            if (a._id < b._id)
+                return 1;
+            return 0;
+        }
+
+        data.sort(compare);
+        $.each(data, function () {
+            tableContent += '<tr>';
+            tableContent += '<td><a href="order/' + this._id + '">' + this._id + '</a></td>';
+            tableContent += '<td>' + this.orderDate + '</td>';
+            tableContent += '<td>' + this.shipName + '</td>';
+            tableContent += '<td>' + this.shipAddress + '</td>';
+            tableContent += editAndDelete();
+            tableContent += '</tr>';
+        });
+        // Inject the whole content string into our existing HTML table
+        $('#orderViewer table tbody').html(tableContent);
+    });
+}
+
+function fillOrderDetails() {
+    // Empty content string
+    var tableContent = '';
+    // jQuery AJAX call for JSON
+    var path = window.location.pathname.split('/');
+    $.getJSON('/orderdetails/' + path[2], function (data) {
+        $("#ID").text("ID: " + data.order._id);
+        $("#date").text("Date: " + data.order.orderDate);
+        $("#requ").text("Required: " + data.order.requiredDate);
+        $("#ship").text("Shipped: " + data.order.shippedDate);
+        $("#employee").text(data.employee.firstName + ' ' + data.employee.lastName).prop('href', '/employee/' + data.employee._id);
+        $("#name").text(data.customer.companyName).prop('href', '/customer/' + data.customer._id);
+        $("#addr").text("Address: " + data.customer.address)
+        $("#zip").text("Post Code: " + data.customer.postalCode);
+        $("#country").text("Country: " + data.customer.country);
+
+        //solution: get all the info from the data
+        //each piece of info will be a paragraph handled in the javascript
+        //the paragraphs will be "hard coded" p1 = "ID: " + data.id;
+        //gl hf
+        var total = 0;
+        $.each(data.orderDetails, function (index, value) {
+            total += (value.unitPrice - value.unitPrice * value.discount) * value.quantity;
+            tableContent += '<tr>';
+            tableContent += '<td>' + data.products[value.productId].name + '</td>';     //this needs to be changed to product name after Tobias' push and our merge
+            tableContent += '<td>' + value.unitPrice.toFixed(2) + '</td>';
+            tableContent += '<td>' + value.quantity + '</td>';
+            tableContent += '<td>' + value.discount.toFixed(2) + '</td>';
+            tableContent += '<td>' + ((value.unitPrice - value.unitPrice * value.discount) * value.quantity).toFixed(2) + '</td>';
+            tableContent += '</tr>';
+        });
+        tableContent += '<tr>';
+        tableContent += '<td>' + 'Total:' + '</td>';
+        tableContent += '<td> </td>';
+        tableContent += '<td> </td>';
+        tableContent += '<td> </td>';
+        tableContent += '<td>' + total.toFixed(2) + ' </td>';
+        tableContent += '</tr>';
+        $('#productsViewer table tbody').html(tableContent);
+    });
+}
+
+function getDetailsAboutEmployee() {
+    var ar = document.URL.split('/');
+    var id = ar[ar.length - 1];
+    $.getJSON('getemployee/' + id, function (data) {
+        var add = "";
+        add += "<tr>";
+        add += "<td>First Name: </td>";
+        add += "<td>" + data.firstName + "</td>";
+        add += "</tr>";
+        add += "<tr>";
+        add += "<td>Last Name: </td>";
+        add += "<td>" + data.lastName + "</td>";
+        add += "</tr>";
+        add += "<tr>";
+        add += "<td>City: </td>";
+        add += "<td>" + data.city + "</td>";
+        add += "</tr>";
+        add += "<tr>";
+        add += "<td>Address: </td>";
+        add += "<td>" + data.address + "</td>";
+        add += "</tr>";
+        add += "<tr>";
+        add += "<td>Region: </td>";
+        add += "<td>" + data.region + "</td>";
+        add += "</tr>";
+        add += "<tr>";
+        add += "<td>Postal Code: </td>";
+        add += "<td>" + data.postalCode + "</td>";
+        add += "</tr>";
+        add += "<tr>";
+        add += "<td>Country: </td>";
+        add += "<td>" + data.country + "</td>";
+        add += "</tr>";
+        add += "<tr>";
+        add += "<td>Phone: </td>";
+        add += "<td>" + data.homePhone + "</td>";
+        add += "</tr>";
+        add += "<tr>";
+        add += "<td>Title: </td>";
+        add += "<td>" + data.title + "</td>";
+        add += "</tr>";
+        add += "<tr>";
+        add += "<td>Title of Courtesy: </td>";
+        add += "<td>" + data.titleOfCourtesy + "</td>";
+        add += "</tr>";
+        add += "<tr>";
+        add += "<td>Birth Date: </td>";
+        add += "<td>" + data.birthDate + "</td>";
+        add += "</tr>";
+        add += "<tr>";
+        add += "<td>Hire Date: </td>";
+        add += "<td>" + data.hireDate + "</td>";
+        add += "</tr>";
+        add += "<tr>";
+        add += "<td>Extension: </td>";
+        add += "<td>" + data.extension + "</td>";
+        add += "</tr>";
+        add += "<tr>";
+        add += "<td>Notes: </td>";
+        add += "<td>" + data.notes + "</td>";
+        add += "</tr>";
+        $('#userTable table tbody').html(add);
+    })
+}
+
+function getDetailsAboutUser() {
+
+    var ar = document.URL.split('/');
+    var id = ar[ar.length - 1];
+
+    $.getJSON('getcustomer/' + id, function (data) {
+        var add = "";
+        add += "<tr>";
+        add += "<td>Name: </td>";
+        add += "<td>" + data._id + "</td>";
+        add += "</tr>";
+        add += "<tr>";
+        add += "<td>City: </td>";
+        add += "<td>" + data.city + "</td>";
+        add += "</tr>";
+        add += "<tr>";
+        add += "<td>Address: </td>";
+        add += "<td>" + data.address + "</td>";
+        add += "</tr>";
+        add += "<tr>";
+        add += "<td>Region: </td>";
+        add += "<td>" + data.region + "</td>";
+        add += "</tr>";
+        add += "<tr>";
+        add += "<td>Postal Code: </td>";
+        add += "<td>" + data.postalCode + "</td>";
+        add += "</tr>";
+        add += "<tr>";
+        add += "<td>Country: </td>";
+        add += "<td>" + data.country + "</td>";
+        add += "</tr>";
+        add += "<tr>";
+        add += "<td>Phone: </td>";
+        add += "<td>" + data.phone + "</td>";
+        add += "</tr>";
+        add += "<tr>";
+        add += "<td>Fax: </td>";
+        add += "<td>" + data.fax + "</td>";
+        add += "</tr>";
+        add += "<tr>";
+        add += "<td>Company name: </td>";
+        add += "<td>" + data.companyName + "</td>";
+        add += "</tr>";
+        add += "<tr>";
+        add += "<td>Contact name: </td>";
+        add += "<td>" + data.contactName + "</td>";
+        add += "</tr>";
+        add += "<tr>";
+        add += "<td>Contact title: </td>";
+        add += "<td>" + data.contactTitle + "</td>";
+        add += "</tr>";
+
+        $('#user table tbody').html(add);
+    })
 }
