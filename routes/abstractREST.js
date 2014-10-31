6c523fe6cf3804d2339821c1322a3c178851e050
Martin Gergov <martingergov1@gmail.com>
2014-10-31 12:42:34 +0100

    REST API generator compleate. Needs testing.

--- routes/abstractREST.js
+++ routes/abstractREST.js
@@ -1,44 +1,70 @@
 var express = require('express');
 
-// Create a REST API handling
-// 
+/*
+  REST API generator that's cooler than all yo' stupid
+  repeating retarded code.
+  / -> GET -> all -> json list of objects
+  /:id -> GET -> get one -> json object
+  /:id -> PUT -> update one -> {"numAffected": <x>}
+  /:id -> DELETE -> delete one -> json object
+
+  The intermediate step between constructing and loading
+  the methods in router(a.k.a produce) allows for modifications
+  of the existing defaults.
+*/
+
 function ExpressREST(facadePath, errorHandlePath) {
     this.router = express.Router();
     this.facade = require(facadePath);
-    this.errorHandle = require(errorHandlePath);
+    this.router.get.facade = this.facade;
+    this.errorHandler = require(errorHandlePath);
     this.all = function(){
+	var facade = this.facade;
+	var errorHandler = this.errorHandler;
 	this.router.get('/', function(req, res) {
-	    this.facade.all(function (err, allOrders) {
-		if(!this.errorHandler.errorHandle(err, res)) return;
+	    facade.all(function (err, elements) {
+		if(!errorHandler.errorHandle(err, res)) return;
 		res.setHeader('Content-Type', 'application/json');
-		res.end(JSON.stringify(allOrders));
+		res.end(JSON.stringify(elements));
 	    })
 	});    
     };
 
     this.get = function(){
+	var facade = this.facade;
+	var errorHandler = this.errorHandler;
 	this.router.get('/:id', function (req, res) {
-	    res.render('orderDetails');
+	    var id = req.params.id;
+	    facade.get(id, function (err, el) {
+		if (!errorHandler.errorHandle(err, res)) return;
+		res.setHeader('Content-Type', 'application/json');
+		res.end(JSON.stringify(el));
+	    });
 	});
     };
 
     this.update = function(){
+	var facade = this.facade;
+	var errorHandler = this.errorHandler;
 	this.router.put('/:id', function (req, res) {
-	    var orderId = req.params.id;
+	    var id = req.params.id;
 	    try {
 		var obj = JSON.parse(req.rawBody);
-		obj._id = orderId;
-		this.facade.update(obj, function(err, numAffected) {
-		    if (!this.errorHandler.errorHandle(err, res)) return;
+		obj._id = id;
+		facade.update(obj, function(err, numAffected) {
+		    if (!errorHandler.errorHandle(err, res))
+			return;
 		    if (numAffected == 0) {
+			var errm = "Document not modified.";
 			res.status(200).send({status: 200,
-					      message: "Document not modified.",
+					      message: errm,
 					      type: 'Not Modified'});
 			res.end();
 			return;
 		    }
 		    res.setHeader('Content-Type', 'application/json');
-		    res.end(JSON.stringify({"numAffected": numAffected}));
+		    res.end(JSON.stringify(
+			{"numAffected": numAffected}));
 		});
 	    } catch (e) {
 		console.log(e);
@@ -51,10 +77,13 @@ function ExpressREST(facadePath, errorHandlePath) {
     };
 
     this.delete = function() {
+	var facade = this.facade;
+	var errorHandler = this.errorHandler;
 	this.router.delete('/:id', function (req, res) {
-	    var orderId = req.params.id;
-	    this.facade.del(orderId, function(err, doc) {
-		if (!errorHandler.errorHandle(err, res)) return;
+	    var id = req.params.id;
+	    facade.del(id, function(err, el) {
+		if (!errorHandler.errorHandle(err, res))
+		    return;
 		if (!doc) {
 		    res.status(404).send({status: 404,
 					  message: "Object not found",
@@ -63,18 +92,17 @@ function ExpressREST(facadePath, errorHandlePath) {
 		    return;
 		}
 		res.setHeader('Content-Type', 'application/json');
-		res.end(JSON.stringify(doc));
+		res.end(JSON.stringify(el));
 	    });
 	});
-
     };
+
+    this.produce = function() {
+	this.all();
+	this.get();
+	this.update();
+	this.delete();
+    }
 }
-
-
-
-
-
-
-
 
 module.exports = {ExpressREST: ExpressREST};
